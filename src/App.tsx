4ddefd75626a3b74b09c2e9d4a93f52288826e55
Joo <beloved930223@gmail.com>
2021-08-18 16:13:50 +0900

    code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import AppRouter from './components/Router';
 import { observer } from 'mobx-react';
--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from '../../pages/Main';
 import Login from '../../pages/Login';
 import Navigation from '../Navigation';
-import UserStore from '../../store/userInfo';
 interface Props {
 	isLoggedIn: boolean;
 }
-// const userStore = new UserStore();
 
 export default function AppRouter(props: Props): JSX.Element {
 	return (
