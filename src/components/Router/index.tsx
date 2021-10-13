99962317832dc7492f6b322722d9ba18469b421b
서주현 <jhseo@rsupport.com>
2021-10-13 18:38:57 +0900

    인기도서 페이지 생성

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from '../../pages/Main';
 import Login from '../../pages/Login';
 import Navigation from '../Navigation';
+import FavoriteBook from '../../pages/BestBook';
 interface Props {
 	isLoggedIn: boolean;
 }
@@ -14,14 +14,17 @@ export default function AppRouter(props: Props): JSX.Element {
 			<Switch>
 				<>
 					{props.isLoggedIn ? (
-						<Route path="/">
+						<Route exact path="/">
 							<Main></Main>
 						</Route>
 					) : (
-						<Route path="/">
+						<Route exact path="/">
 							<Login></Login>
 						</Route>
 					)}
+					<Route path="/bestbook">
+						<FavoriteBook></FavoriteBook>
+					</Route>
 				</>
 			</Switch>
 		</Router>
---
+++ src/pages/BestBook/index.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		paperContainer: {
+			position: 'relative',
+			display: 'flex',
+			justifyContent: 'center',
+			flexWrap: 'wrap',
+			'& > *': {
+				margin: theme.spacing(3),
+			},
+		},
+
+		thumbnail: {
+			width: 150,
+			height: 200,
+		},
+	}),
+);
+const BestBook = (): JSX.Element => {
+	const classes = useStyles();
+	const [bookList, setBookList] = useState([]);
+	useEffect(() => {
+		axios.get(`${process.env.REACT_APP_BORROW_URI}?bestbook=true`).then(res => {
+			setBookList(res.data);
+		});
+	}, []);
+	const renderList = () => {
+		return bookList.map((data: any, i: number) => {
+			return (
+				<Paper key={i}>
+					<img className={classes.thumbnail} src={data.poster}></img>
+					<p>{data.name}</p>
+					<p>{data.cnt}회 대여 되었어요</p>
+				</Paper>
+			);
+		});
+	};
+	return (
+		<div>
+			<div style={{ position: 'relative' }}>인기..도서..! Top3</div>
+			<div className={classes.paperContainer}>{renderList()}</div>
+		</div>
+	);
+};
+
+export default BestBook;
