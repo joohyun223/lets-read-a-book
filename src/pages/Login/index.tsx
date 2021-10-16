9e918eb59f70834d442d48e4d0e515cbc165eeb1
서주현 <jhseo@rsupport.com>
2021-10-17 03:06:44 +0900

    로그인화면 ui 변경

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,8 @@
 import { GoogleLogin } from 'react-google-login';
 import user from '../../store/userInfo';
 import { observer } from 'mobx-react';
+import { Typography } from '@material-ui/core';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 
 const Login = (): JSX.Element => {
 	const responseGoogle = (resp: any) => {
@@ -23,16 +25,29 @@ const Login = (): JSX.Element => {
 	};
 
 	return (
-		<>
-			<div>로그인이 필요합니다.</div>
+		<div
+			style={{ backgroundColor: '#ebf6ff', position: 'absolute', width: '100%', height: '100%' }}
+		>
+			<MenuBookIcon style={{ marginTop: '200px', fontSize: '200px', color: '#5e7fb9' }} />
+			<Typography
+				variant="h5"
+				style={{
+					justifyContent: 'center',
+					alignItems: 'center',
+					display: 'flex',
+					margin: '20px 0px 10px 0px ',
+				}}
+			>
+				~책을 읽읍시다~
+			</Typography>
 			<GoogleLogin
 				clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID + ''}
-				buttonText="로그인"
+				buttonText="알서포트 이메일로 로그인"
 				onSuccess={responseGoogle}
 				onFailure={responseGoogle}
 				cookiePolicy={'single_host_origin'}
 			/>
-		</>
+		</div>
 	);
 };
 export default observer(Login);
