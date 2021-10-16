39b819eaa723a7e904851e3aaae0e531aad96a66
서주현 <jhseo@rsupport.com>
2021-10-17 02:48:43 +0900

    라우트 수정

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -3,6 +3,7 @@ import Main from '../../pages/Main';
 import Login from '../../pages/Login';
 import Navigation from '../Navigation';
 import FavoriteBook from '../../pages/BestBook';
+import BestPerson from '../../pages/BestPerson';
 interface Props {
 	isLoggedIn: boolean;
 }
@@ -14,17 +15,22 @@ export default function AppRouter(props: Props): JSX.Element {
 			<Switch>
 				<>
 					{props.isLoggedIn ? (
-						<Route exact path="/">
-							<Main></Main>
-						</Route>
+						<>
+							<Route exact path="/">
+								<Main></Main>
+							</Route>
+							<Route path="/bestbook">
+								<FavoriteBook></FavoriteBook>
+							</Route>
+							<Route path="/bestperson">
+								<BestPerson></BestPerson>
+							</Route>
+						</>
 					) : (
-						<Route exact path="/">
+						<Route path="/">
 							<Login></Login>
 						</Route>
 					)}
-					<Route path="/bestbook">
-						<FavoriteBook></FavoriteBook>
-					</Route>
 				</>
 			</Switch>
 		</Router>
